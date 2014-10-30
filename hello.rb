000d120f956dabe57d1703ab0e8cd37c13fab36b
root <root@chef-server.(none)>
2014-10-30 22:33:33 +0530

    ab

--- hello.rb
+++ hello.rb
@@ -3,9 +3,9 @@ puts "Hello, Let's start with Ruby!"
 puts "Enter your name?"
 #name = gets.chomp
 #puts "Hi #{name}"
-<<<<<<< HEAD
+
 puts "new line added"
-=======
+
 my_arr = {
  :a => 1,
  :b => 2,
@@ -13,4 +13,3 @@ my_arr = {
  }
  puts my_arr[:a]
 
->>>>>>> 05345073d6b75e56664f1205122b99ea61e0c9ee
